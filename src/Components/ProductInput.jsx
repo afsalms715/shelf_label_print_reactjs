fd7905c23a830e76e1f661a3170dc0e70df2cbf0
afsalms715 <afsalms715@gamil.com>
2023-08-22 22:04:21 +0300

    data fetching and table creation

--- src/Components/ProductInput.jsx
+++ src/Components/ProductInput.jsx
@@ -3,18 +3,51 @@ import './Styles/ProductInput.css'
 
 const ProductInput = () => {
     const [loc,setLoc]=useState(701);
-    const [barcode,setBarcode]=useState();
+    const [barcode,setBarcode]=useState("");
     const [sudesc,setSudesc]=useState("");
+    const [sudescAr,setSudescAr]=useState("");
+    const[rsp,setRsp]=useState("")
+    const [products,setProducts]=useState([])
     const getProductDtl= (e)=>{
-        fetch("https://localhost:44391/api/ProductDtl/product?barcode=1415&loc=701").then(resp=>resp.json()).then(resp=>{
+        fetch(`https://localhost:7168/api/Product/GetProduct?Loc=${loc}&Barcode=${barcode}`).then(resp=>resp.json()).then(resp=>{
             console.log(resp)
+            if(resp.status){
+                setSudesc("Not Found")
+            }else{
+                setSudesc(resp.suDesc)
+                setSudescAr(resp.suDesc) 
+                setRsp(resp.rsp)         
+            }
         })
     }
+
     useEffect(()=>{
-        console.log(loc)
-        console.log(barcode)
-        console.log(sudesc)
-    },[loc,barcode,sudesc])
+        if(barcode!=''){
+            getProductDtl() 
+        }    
+    },[barcode])
+
+    useEffect(()=>{
+        if(sudesc=='Not Found'){
+            document.getElementById("sudesc").style.border="1px solid red"
+        }else{
+            document.getElementById("sudesc").style.border=""
+        }
+    },[sudesc])
+
+    const addProducts=()=>{
+        if(products.length<6){
+            if(sudesc!=''){
+                products.push({"barcode":barcode,"suDesc":sudesc,"sudescAr":sudescAr,"rsp":rsp})
+                setBarcode('')
+                setSudesc('')
+                setRsp('')
+                console.log(products)
+            }
+        }else{
+            alert("Maximum 6 Products")
+        }
+    }
 
   return (
     <div className='ms-auto me-auto'>
@@ -24,6 +57,12 @@ const ProductInput = () => {
                 <select className='form-select form-select-sm' onChange={(e)=>setLoc(e.target.value)}>
                     <option value="701">Grand Mall</option>
                     <option value="702">Ezdan</option>
+                    <option value="703">Plaza 2</option>
+                    <option value="704">Plaza 3</option>
+                    <option value="705">Shahaniya</option>
+                    <option value="706">Barwa</option>
+                    <option value="707">Aziziya</option>
+                    <option value="708">Umm Garn</option>
                 </select>
             </div>
             <div className='col-md-3'>
@@ -32,9 +71,9 @@ const ProductInput = () => {
             </div>
             <div className='col-md-5'>
                 <label>Su Description</label>
-                <input type='text' placeholder='su description' className='form-control form-control-sm ' value={sudesc} readOnly/>
+                <input type='text' id='sudesc' placeholder='su description' className='form-control form-control-sm ' value={sudesc} readOnly/>
             </div>
-            <button className='btn btn-primary btn-sm col-md-2' style={{height:"30px",marginTop:"23px"}} onClick={(e)=>getProductDtl(e)}>ADD</button>
+            <button className='btn btn-primary btn-sm col-md-2' style={{height:"30px",marginTop:"23px"}} onClick={addProducts}>ADD</button>
         </div>
         <div className='row p-md-3'>
             <div className='col'>
@@ -48,23 +87,14 @@ const ProductInput = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                        </tr>
-                        <tr>
-                        <th scope="row">3</th>
-                        <td colSpan="2">Larry the Bird</td>
-                        <td>@twitter</td>
-                        </tr>
+                        {products.map((resp,index)=>
+                            <tr key={index}>
+                                <td>{resp.barcode}</td>
+                                <td>{resp.suDesc}</td>
+                                <td>{resp.sudescAr}</td>
+                                <td>{resp.rsp}</td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </div>
@@ -73,17 +103,20 @@ const ProductInput = () => {
         <div className='row'>
             <div className='d-flex justify-content-center'>
                 <div className="shelftag border" id="contents">
-                    <div className="block block-bg">
+                    {
+                    products.map((resp,index)=>
+                    <div key={index} className="block block-bg">
                         <div className="prod-container  d-flex">
                             <div className="desc ">
-                                <small className="desc-ar desc-text" >test</small>
-                                <small className="desc-en desc-text">test</small>
+                                <small className="desc-ar desc-text" >{resp.suDesc}</small>
+                                <small className="desc-en desc-text">{resp.suDesc}</small>
                             </div>
                             <div className="price-shelf">
-                                <small className="price-nor">12</small>
+                                <small className="price-nor">{resp.rsp} | <span className='ar-price'>{resp.rsp}</span></small>
                             </div>
                         </div>
-                    </div>                       
+                    </div> ) 
+                    }
                 </div>  
             </div>
         </div>
